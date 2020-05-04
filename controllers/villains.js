034dbd3d929d81ccbc1080a36a894c9d5da009ea
Austin Bliss <abliss2@live.com>
2020-05-04 18:04:02 -0400

    happily ever after trial has been created, not so happily ever after has also been acounted for

--- controllers/villains.js
+++ controllers/villains.js
@@ -1,39 +1,45 @@
 const models = require('../models')
 
 const getAllVillains = async (request, response) => {
-  const teams = await models.villains.findAll({ attributes: ['name', 'movie', 'slug'] })
+  try {
+    const teams = await models.villains.findAll({ attributes: ['name', 'movie', 'slug'] })
 
-  return response.send(teams)
+    return response.send(teams)
+  } catch (error) { return response.status(500).send('Cant make contact with villains, Perhaps another try?') }
 }
 
 const getVillainBySlug = async (request, response) => {
-  const { slug } = request.params
+  try {
+    const { slug } = request.params
 
-  const findVillainSlug = await models.villains.findOne({
-    where: { slug: slug },
-    attributes: ['name', 'movie', 'slug']
-  })
+    const findVillainSlug = await models.villains.findOne({
+      where: { slug: slug },
+      attributes: ['name', 'movie', 'slug']
+    })
 
-  return findVillainSlug
-    ? response.send(findVillainSlug)
-    : response.sendStatus(404)
+    return findVillainSlug
+      ? response.send(findVillainSlug)
+      : response.sendStatus(404)
+  } catch (error) { return response.status(500).send('Cant make contact with villains, Perhaps another try?') }
 }
 
 const addNewVillain = async (request, response) => {
-  const {
-    name, movie, slug
-  } = request.body
+  try {
+    const {
+      name, movie, slug
+    } = request.body
 
-  if (!name || !movie || !slug) {
-    return response.status(400).send('Please make sure all fields are filled')
-  }
+    if (!name || !movie || !slug) {
+      return response.status(400).send('Please make sure all fields are filled')
+    }
 
-  const newVillain = await models.villains.create({
-    name, movie, slug
-  })
+    const newVillain = await models.villains.create({
+      name, movie, slug
+    })
 
 
-  return response.status(201).send(newVillain)
+    return response.status(201).send(newVillain)
+  } catch (error) { return response.status(500).send('Cant make contact with villains, Perhaps another try?') }
 }
 
 module.exports = { getAllVillains, getVillainBySlug, addNewVillain }
---
+++ tests/mocks/villains.js
@@ -0,0 +1,24 @@
+const villainList = [{
+  name: 'Scar',
+  movie: 'The Lion King',
+  slug: 'scar',
+}, {
+  name: 'Shan Yu',
+  movie: 'Mulan',
+  slug: 'shan-yu',
+
+}]
+
+const singleVillain = {
+  name: 'Horned King',
+  movie: 'The Black Cauldron',
+  slug: 'horned-king',
+}
+
+const postedVillain = {
+  name: 'Horned King',
+  movie: 'The Black Cauldron',
+  slug: 'horned-king',
+}
+
+module.exports = { villainList, singleVillain, postedVillain }
